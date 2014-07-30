df7aaee88107d1c6959d9ab5491e74cf9e1557f3
Camsson <wbushlyh@163.com>
2014-07-30 17:31:17 +0800

    initial commit comment

---
+++ CanceledOnTouchOutside.java
@@ -0,0 +1,2 @@
+//二.AlertDialog 点击外部取消实现代码
+alertDialog.setCanceledOnTouchOutside(true);
---
+++ Scrolling.java
@@ -0,0 +1,4 @@
+//一.实现界面滚动Scrolling代码
+textView.setMovementMethod(ScrollingMovementMethod. getInstance());
+
+
